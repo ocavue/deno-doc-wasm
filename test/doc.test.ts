ee24b2d679b1eb3e316e74ea57d3ba1ff4a05b99
ocavue <ocavue@gmail.com>
2026-02-21 02:35:47 +1100

    update test

--- test/doc.test.ts
+++ test/doc.test.ts
@@ -1,10 +1,29 @@
-import { expect, it } from 'vitest'
+import {  it, assert } from 'vitest'
 
 import { doc } from '../dist/index.js'
 
 it('can parse from deno.land/std/fmt/colors.ts', async () => {
-
-  const colorsDoc = await doc(["https://deno.land/std/fmt/colors.ts"]);
-
-  expect(colorsDoc).toMatchInlineSnapshot()
+  const records = await doc(
+    ["https://deno.land/std@0.104.0/fmt/colors.ts"],
+  );
+  const entries = records["https://deno.land/std@0.104.0/fmt/colors.ts"];
+  assert.equal(entries.length, 49);
+  const fnStripColor = entries.find((n) =>
+    n.kind === "function" && n.name === "stripColor"
+  );
+  assert(fnStripColor, "unable to locate specific node");
+  assert(fnStripColor.kind === "function");
+  assert(fnStripColor.functionDef);
+  assert.deepEqual(fnStripColor.functionDef.params, [{
+    kind: "identifier",
+    name: "string",
+    optional: false,
+    tsType: {
+      repr: "string",
+      kind: "keyword", 
+      keyword: "string",
+    },
+  }]);
 })
+
+
