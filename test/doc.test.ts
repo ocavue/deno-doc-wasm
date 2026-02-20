e1dfe0d634e6fdbfbe59c951b9cbc8c7367aa189
ocavue <ocavue@gmail.com>
2026-02-21 02:50:35 +1100

    update

--- test/doc.test.ts
+++ test/doc.test.ts
@@ -1,10 +1,11 @@
-import { it, assert } from 'vitest'
+import { it, assert, expect } from 'vitest'
 
 import { doc } from '../dist/index.js'
 
 it('can parse from deno.land/std/fmt/colors.ts', async () => {
-  const records = await doc(['https://deno.land/std@0.104.0/fmt/colors.ts'])
-  const entries = records['https://deno.land/std@0.104.0/fmt/colors.ts']
+  const url = 'https://deno.land/std@0.104.0/fmt/colors.ts'
+  const records = await doc([url])
+  const entries = records[url]
   assert.equal(entries.length, 49)
   const fnStripColor = entries.find(
     (n) => n.kind === 'function' && n.name === 'stripColor',
@@ -24,4 +25,147 @@ it('can parse from deno.land/std/fmt/colors.ts', async () => {
       },
     },
   ])
+})
+
+
+it('can parse from unpkg.com', async () => {
+  const url = 'https://unpkg.com/@ocavue/utils@1.5.0'
+  const records = await doc([url])
+  const entries = records[url]
+  console.log("entries", entries)
+  expect(entries.map(entry => ({
+    name: entry.name,
+    kind: entry.kind,
+  }))).toMatchInlineSnapshot(`
+    [
+      {
+        "kind": "variable",
+        "name": "Counter",
+      },
+      {
+        "kind": "variable",
+        "name": "DefaultMap",
+      },
+      {
+        "kind": "variable",
+        "name": "DefaultWeakMap",
+      },
+      {
+        "kind": "variable",
+        "name": "WeakCounter",
+      },
+      {
+        "kind": "function",
+        "name": "formatBytes",
+      },
+      {
+        "kind": "function",
+        "name": "getDocument",
+      },
+      {
+        "kind": "function",
+        "name": "getDocumentElement",
+      },
+      {
+        "kind": "function",
+        "name": "getId",
+      },
+      {
+        "kind": "function",
+        "name": "getWindow",
+      },
+      {
+        "kind": "function",
+        "name": "isDeepEqual",
+      },
+      {
+        "kind": "function",
+        "name": "isDocument",
+      },
+      {
+        "kind": "function",
+        "name": "isDocumentFragment",
+      },
+      {
+        "kind": "function",
+        "name": "isElement",
+      },
+      {
+        "kind": "function",
+        "name": "isElementLike",
+      },
+      {
+        "kind": "function",
+        "name": "isHTMLElement",
+      },
+      {
+        "kind": "function",
+        "name": "isMap",
+      },
+      {
+        "kind": "function",
+        "name": "isMathMLElement",
+      },
+      {
+        "kind": "function",
+        "name": "isNodeLike",
+      },
+      {
+        "kind": "function",
+        "name": "isNotNullish",
+      },
+      {
+        "kind": "function",
+        "name": "isObject",
+      },
+      {
+        "kind": "function",
+        "name": "isSVGElement",
+      },
+      {
+        "kind": "function",
+        "name": "isSet",
+      },
+      {
+        "kind": "function",
+        "name": "isShadowRoot",
+      },
+      {
+        "kind": "function",
+        "name": "isTextNode",
+      },
+      {
+        "kind": "function",
+        "name": "isWindowLike",
+      },
+      {
+        "kind": "function",
+        "name": "mapGroupBy",
+      },
+      {
+        "kind": "function",
+        "name": "mapValues",
+      },
+      {
+        "kind": "function",
+        "name": "objectEntries",
+      },
+      {
+        "kind": "function",
+        "name": "objectGroupBy",
+      },
+      {
+        "kind": "function",
+        "name": "once",
+      },
+      {
+        "kind": "function",
+        "name": "sleep",
+      },
+      {
+        "kind": "variable",
+        "name": "supportsRegexLookbehind",
+      },
+    ]
+  `)
 })
